1859d1794e084e27bf4fd8798f78cd1e4b202102
Ryan Wibawa <ryanwibawa000@gmail.com>
2025-03-13 02:47:16 +0700

    Adding view engine to ejs

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ var isCorrect;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 function checkHighScore(isCorrect) {
   if (score >= high_score) {
     high_score = score;
